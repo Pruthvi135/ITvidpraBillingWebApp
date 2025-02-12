787a16c6ab35a4ea0d9a4ad17ec8966a7c7d390f
Pruthvi <pruthviraut531@gmail.com>
2025-02-13 04:20:45 +0530

    Add project files.

---
+++ BillingWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BillingWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
